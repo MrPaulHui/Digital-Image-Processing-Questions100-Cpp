2680b6006eae6bd746999c73dd06af897ca7dcb3
MrPaulHui <paulhuiabc@163.com>
2020-05-06 00:26:09 +0800

    perfect add face align

--- t28-31_more_face_align.cpp
+++ t28-31_more_face_align.cpp
@@ -25,7 +25,6 @@ cv::Mat Face_Align_Affine(cv::Mat img, cv::Point* lmks, cv::Point* standard_lmks
         X.at<double>(i*2+1,3) = 1;
     }
     A = (X.t()*X).inv()*X.t()*Y;
-    cout<<A.rows<<" "<<A.cols<<endl;
     double sc, ss, tx, ty;
     sc = A.at<double>(0,0);
     ss = A.at<double>(1,0);
@@ -35,6 +34,40 @@ cv::Mat Face_Align_Affine(cv::Mat img, cv::Point* lmks, cv::Point* standard_lmks
     return Affine(img, sc,-ss,ss,sc,tx,ty,out_size,out_size);
 }
 
+cv::Mat face_align(cv::Mat img, cv::Point* lmks, cv::Point* standard_lmks, int out_size, int lmk_num=5){
+    //Affine调用的库函数，仅此而已的区别，而且似乎并没有自己实现的快多少
+    cv::Mat X = cv::Mat::zeros(lmk_num*2, 4, CV_64FC1);
+    cv::Mat Y = cv::Mat::zeros(lmk_num*2, 1, CV_64FC1);
+    cv::Mat A;
+    for(int i=0;i<lmk_num;i++){
+        Y.at<double>(i*2,0) = standard_lmks[i].x;
+        Y.at<double>(i*2+1,0) = standard_lmks[i].y;
+        X.at<double>(i*2,0) = lmks[i].x;
+        X.at<double>(i*2,1) = -lmks[i].y;
+        X.at<double>(i*2,2) = 1;
+        X.at<double>(i*2,3) = 0;
+        X.at<double>(i*2+1,0) = lmks[i].y;
+        X.at<double>(i*2+1,1) = lmks[i].x;
+        X.at<double>(i*2+1,2) = 0;
+        X.at<double>(i*2+1,3) = 1;
+    }
+    A = (X.t()*X).inv()*X.t()*Y;
+    double sc, ss, tx, ty;
+    sc = A.at<double>(0,0);
+    ss = A.at<double>(1,0);
+    tx = A.at<double>(2,0);
+    ty = A.at<double>(3,0);
+    cout<<"align_matrix: a: "<<sc<<" b: "<<-ss<<" c: "<<ss<<" d: "<<sc<<" tx: "<<tx<<" ty: "<<ty<<endl;
+    double Mm[2][3]={
+        {sc,-ss,tx},
+        {ss,sc,ty},
+    };
+    cv::Mat M(2,3,CV_64FC1,Mm);
+    cv::Mat out;
+    cv::warpAffine(img, out, M, cv::Size(out_size,out_size));
+    cv::Mat out_ = out.clone();
+    return out_;
+}
 int main(){
     cv::Mat img = cv::imread("../imgs/face.jpg", cv::IMREAD_COLOR);
     cv::Point lmks[5];
@@ -50,6 +83,7 @@ int main(){
     slmks[3] = cv::Point(42,93);
     slmks[4] = cv::Point(71,93); // 112*112模板的标注landmark
     cv::Mat out = Face_Align_Affine(img, lmks, slmks, 112);
+    //cv::Mat out = face_align(img, lmks, slmks, 112);
     cv::imshow("align_face", out);
     cv::waitKey(0);
     cv::destroyAllWindows();
